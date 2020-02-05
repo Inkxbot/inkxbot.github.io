a1e073dc553675d646fdfe2bd5dbe19daf697e39
Inkx <inkxthesquid@mail.com>
2020-02-05 01:16:03 -0800

    add mobile support

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 
 window.onload=function() {
+	var webformat = null;
 	var footer = document.createElement("footer");
 	var footText = document.createTextNode("© 2017-2020 please contact @InkxtheSquid#0069 on discord for bugs and kinks");
 	footer.appendChild(footText);
-	return document.getElementById("copyright").appendChild(footer);
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    	document.getElementById('uaf_client_css-css').href = "assets/mobile.css";
+    	document.getElementById("helpAndAdd2").size='400%';
+    	document.getElementById("helpAndAdd1").size='400%';
+    }
+	return document.getElementById("copyright").appendChild(footer)&&webformat;
 }
